f508431bba022bc781df9b71bcbe2587cb6de379
John Dzikunu <56548679+tekpor@users.noreply.github.com>
2023-04-28 23:47:03 +0000

    Initial commit: added Tic Tac Toe game
    This commit adds a complete Tic Tac Toe game built with Angular. The game allows two players to take turns clicking on cells in a 3x3 grid until one player gets three in a row (horizontally, vertically, or diagonally) or all cells are filled without any player getting three in a row. The game also keeps track of the number of wins for each player and displays a simple leaderboard at the bottom of the screen.
    
    To run the game, clone the repository, navigate to the project directory, install the required dependencies using
    pm install, and run the application using
    g serve. Then open a web browser and go to the URL http://localhost:4200 to play the game.
    
    This project is licensed under the MIT License. See the LICENSE file for details.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,54 +1,80 @@
-import { Component } from '@angular/core';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { AppComponent } from './app.component';
 
-enum Cell {
-  Empty,
-  X,
-  O
-}
+describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  player1Name = '';
-  player2Name = '';
-  currentPlayer = Cell.X;
-  board: Cell[][] = [[Cell.Empty, Cell.Empty, Cell.Empty], [Cell.Empty, Cell.Empty, Cell.Empty], [Cell.Empty, Cell.Empty, Cell.Empty]];
-  gameOver = false;
-  players = [
-    { name: '', wins: 0 },
-    { name: '', wins: 0 }
-  ];
-  gameStarted = false;
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [AppComponent],
+    }).compileComponents();
+  });
 
-  get currentPlayerName(): string {
-    return this.currentPlayer === Cell.X ? this.player1Name : this.player2Name;
-  }
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-  startGame(): void {
-    this.players[0].name = this.player1Name;
-    this.players[1].name = this.player2Name;
-    this.gameStarted = true;
-  }
+  it('should create the app', () => {
+    expect(component).toBeTruthy();
+  });
 
-  cellClicked(row: number, col: number): void {
-    if (this.board[row][col] !== Cell.Empty || this.gameOver) {
-      return;
-    }
+  it('should start with an empty board', () => {
+    expect(component.board.flat().every(cell => cell === 0)).toBeTrue();
+  });
 
-    this.board[row][col] = this.currentPlayer;
+  it('should alternate between X and O on each turn', () => {
+    component.cellClicked(0, 0);
+    expect(component.board[0][0]).toBe(1);
+    expect(component.currentPlayer).toBe(2);
 
-    if (this.checkForWinner()) {
-      this.gameOver = true;
-      this.players[this.currentPlayer].wins++;
-    } else if (this.isBoardFull()) {
-      this.gameOver = true;
-    } else {
-      this.currentPlayer = this.currentPlayer === Cell.X ? Cell.O : Cell.X;
-    }
-  }
+    component.cellClicked(1, 0);
+    expect(component.board[1][0]).toBe(2);
+    expect(component.currentPlayer).toBe(1);
 
-  checkForWinner(): boolean {
-    for (let i = 0; i < 3;
+    component.cellClicked(2, 2);
+    expect(component.board[2][2]).toBe(1);
+    expect(component.currentPlayer).toBe(2);
+  });
+
+  it('should not allow a cell to be clicked twice', () => {
+    component.cellClicked(0, 0);
+    expect(component.board[0][0]).toBe(1);
+
+    component.cellClicked(0, 0);
+    expect(component.board[0][0]).toBe(1);
+  });
+
+  it('should declare a winner when a player gets three in a row', () => {
+    component.cellClicked(0, 0);
+    component.cellClicked(1, 0);
+    component.cellClicked(0, 1);
+    component.cellClicked(1, 1);
+    component.cellClicked(0, 2);
+    expect(component.gameOver).toBeTrue();
+    expect(component.players[0].wins).toBe(1);
+  });
+
+  it('should declare a tie when the board is full', () => {
+    component.cellClicked(0, 0);
+    component.cellClicked(0, 1);
+    component.cellClicked(0, 2);
+    component.cellClicked(1, 1);
+    component.cellClicked(1, 0);
+    component.cellClicked(1, 2);
+    component.cellClicked(2, 1);
+    component.cellClicked(2, 0);
+    component.cellClicked(2, 2);
+    expect(component.gameOver).toBeTrue();
+  });
+
+  it('should reset the board and game state when resetGame is called', () => {
+    component.cellClicked(0, 0);
+    component.resetGame();
+    expect(component.board.flat().every(cell => cell === 0)).toBeTrue();
+    expect(component.currentPlayer).toBe(1);
+    expect(component.gameOver).toBeFalse();
+  });
+});
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,105 @@
 import { Component } from '@angular/core';
 
+enum Cell {
+  Empty,
+  X,
+  O
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Tic-Tac-Toe';
+  player1Name = '';
+  player2Name = '';
+  currentPlayer = Cell.X;
+  board: Cell[][] = [[Cell.Empty, Cell.Empty, Cell.Empty], [Cell.Empty, Cell.Empty, Cell.Empty], [Cell.Empty, Cell.Empty, Cell.Empty]];
+  gameOver = false;
+  players = [
+    { name: '', wins: 0 },
+    { name: '', wins: 0 }
+  ];
+  gameStarted = false;
+  winner: number = 0;
+
+  get currentPlayerName(): string {
+    return this.currentPlayer === Cell.X ? this.player1Name : this.player2Name;
+  }
+  getSymbol(value: number): string {
+    if (value === 1) {
+      return 'X';
+    } else if (value === 2) {
+      return 'O';
+    } else {
+      return '';
+    }
+  }
+  startGame(): void {
+    this.players[0].name = this.player1Name;
+    this.players[1].name = this.player2Name;
+    this.gameStarted = true;
+  }
+
+  cellClicked(row: number, col: number): void {
+    if (this.board[row][col] !== Cell.Empty || this.gameOver) {
+      return;
+    }
+
+    this.board[row][col] = this.currentPlayer;
+
+    if (this.checkForWinner()) {
+      this.gameOver = true;
+      this.players[this.currentPlayer - 1].wins++;
+    } else if (this.isBoardFull()) {
+      this.gameOver = true;
+    } else {
+      this.currentPlayer = this.currentPlayer === Cell.X ? Cell.O : Cell.X;
+    }
+  }
+
+  checkForWinner(): boolean {
+    // Check rows
+    for (let i = 0; i < 3; i++) {
+      if (this.board[i][0] !== Cell.Empty && this.board[i][0] === this.board[i][1] && this.board[i][1] === this.board[i][2]) {
+        return true;
+      }
+    }
+
+    // Check columns
+    for (let i = 0; i < 3; i++) {
+      if (this.board[0][i] !== Cell.Empty && this.board[0][i] === this.board[1][i] && this.board[1][i] === this.board[2][i]) {
+        return true;
+      }
+    }
+
+    // Check diagonals
+    if (this.board[0][0] !== Cell.Empty && this.board[0][0] === this.board[1][1] && this.board[1][1] === this.board[2][2]) {
+      return true;
+    }
+    if (this.board[2][0] !== Cell.Empty && this.board[2][0] === this.board[1][1] && this.board[1][1] === this.board[0][2]) {
+      return true;
+    }
+
+    return false;
+  }
+
+  isBoardFull(): boolean {
+    for (let row of this.board) {
+      for (let cell of row) {
+        if (cell === Cell.Empty) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+
+  resetGame(): void {
+    this.board = [[Cell.Empty, Cell.Empty, Cell.Empty], [Cell.Empty, Cell.Empty, Cell.Empty], [Cell.Empty, Cell.Empty, Cell.Empty]];
+    this.currentPlayer = Cell.X;
+    this.gameOver = false;
+  }
 }
